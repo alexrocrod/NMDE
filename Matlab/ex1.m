0d63c296de1ad65fe53e6303686265811e2312ca
alexrocrod <alexandrerrodrigues@ua.pt>
2021-10-26 15:33:41 +0200

    init

---
+++ Matlab/ex1.m
@@ -0,0 +1,14 @@
+%% Exercise # 1 -  Numerical methods for ODEs
+% Course: Numerical Methods For Differential Equations
+% Name: Alexandre da Rocha Rodrigues
+% Matricola: 2039952
+% November 2021
+
+%% Constants
+
+
+
+
+
+
+%% 
