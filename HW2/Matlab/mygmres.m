459a0fa32cb4e60ee99fcbcfc80bb356227859ed
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-01-06 20:07:16 +0000

    HW2 GMRES refactored V vectir

--- HW2/Matlab/mygmres.m
+++ HW2/Matlab/mygmres.m
@@ -1,25 +1,26 @@
 function [x, iter, resvec, flag] = mygmres(A, b, tol, maxit, x0)
     r = b - A * x0;
-    v = zeros(maxit + 1, length(r));
+    v = zeros(length(r), maxit + 1);
     h = zeros(maxit + 1, maxit + 1);
-    rho = zeros(maxit+1,1);
+    rho = zeros(maxit + 1, 1);
 
     beta = norm(r,2);
     rho(1) = beta;
-    v(1,:) = r / beta;
+    v(:,1) = r' / beta;
     thres = tol * norm(b);
     
     flag = 0;
     k = 0;
     while (rho(k+1) > thres && k < maxit) 
         k = k + 1;
-        v(k+1,:) = A * v(k,:)';
+        v(:,k+1) = A * v(:,k);
+
         for j = 1:k
-            h(j,k) = v(k+1,:) * v(j,:)';
-            v(k+1,:) = v(k+1,:) - h(j,k) * v(j,:);
+            h(j,k) = v(:,k+1)' * v(:,j);
+            v(:,k+1) = v(:,k+1) - h(j,k) * v(:,j);
         end
 
-        h(k+1,k) = norm(v(k+1,:));
+        h(k+1,k) = norm(v(:,k+1));
         H = h(1:k+1, 1:k);
         [Q,R] = qr(H);
         rho(k+1) = abs(beta * Q(1,k+1));
@@ -28,17 +29,17 @@ function [x, iter, resvec, flag] = mygmres(A, b, tol, maxit, x0)
             flag = -1;
             break
         end
-        v(k+1,:) = v(k+1,:) / h(k+1,k);
+        v(:,k+1) = v(:,k+1) / h(k+1,k);
        
         
     end
     e1 = zeros(k,1); e1(1) = 1; be1 = beta * e1;
     if flag == -1
-        y = H\be1;
+        y = H \ be1;
     else
         y = R(1:k,1:k) \ (Q(1:k,1:k)' * be1);
     end
-    x = x0 + v(1:k,:)' * y;
+    x = x0 + v(:,1:k) * y;
 
     resvec = rho(1:k+1); 
     iter = k;
--- HW2/Matlab/myprecgmres.m
+++ HW2/Matlab/myprecgmres.m
@@ -4,26 +4,26 @@ function [x, iter, resvec, flag] = myprecgmres(A, b, tol, maxit, x0, L, U)
     r = r0;
 
     h = zeros(maxit + 1, maxit + 1);
-    v = zeros(maxit + 1, length(r));
+    v = zeros(length(r), maxit + 1);
     rho = zeros(maxit + 1, 1);
 
     beta = norm(r,2);
     rho(1) = beta;
-    v(1,:) = r / beta;     
+    v(:,1) = r' / beta;     
     thres = tol * normest(LU\b);
 
     flag = 0;
     k = 0;
     while (normest(LU\r) > thres && k < maxit)
         k = k + 1;
-        v(k+1,:) = L \ (A * (U \ v(k,:)'));
+        v(:,k+1) = L \ (A * (U \ v(:,k)));
 
         for j = 1:k
-            h(j,k) = v(k+1,:) * v(j,:)';
-            v(k+1,:) = v(k+1,:) - h(j,k) * v(j,:);
+            h(j,k) = v(:,k+1)' * v(:,j);
+            v(:,k+1) = v(:,k+1) - h(j,k) * v(:,j);
         end
 
-        h(k+1,k) = norm(v(k+1,:));
+        h(k+1,k) = norm(v(:,k+1));
         if h(k+1,k) == 0
             flag = -1;
             break
@@ -31,12 +31,12 @@ function [x, iter, resvec, flag] = myprecgmres(A, b, tol, maxit, x0, L, U)
 
         H = h(1:k+1, 1:k);
         [Q,R] = qr(H);
-        v(k+1,:) = v(k+1,:) / h(k+1,k);
+        v(:,k+1) = v(:,k+1) / h(k+1,k);
 
         e1 = zeros(k,1); e1(1) = 1; be1 = beta * e1;
 
         y = R(1:k,1:k) \ (Q(1:k,1:k)' * be1);
-        x = x0 + U \ v(1:k,:)' * y;
+        x = x0 + U \ v(:,1:k) * y;
         r = L \ (b - A * x);
 
         rho(k+1) = norm(r);
@@ -45,7 +45,7 @@ function [x, iter, resvec, flag] = myprecgmres(A, b, tol, maxit, x0, L, U)
         H = h(1:k+1, 1:k);
         e1 = zeros(k,1); e1(1) = 1; be1 = beta * e1;
         y = H \ be1;
-        x = x0 + v(1:k,:)'*y;
+        x = x0 + v(:,1:k)'*y;
     end
     resvec = rho(1:k+1); 
     iter = k;
