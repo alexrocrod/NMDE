657d22e771471292cfaf6817b13de96dd75b68e6
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-01-09 14:34:57 +0000

    HW2 v3 all, missing revision and maybe intros

--- HW2/Matlab/helper.m
+++ HW2/Matlab/helper.m
@@ -1,6 +1,6 @@
-% Extra helper script to compute general data about a sparse matrix 
-% representing the finite difference Laplacian
+% Extra helper script
 
+%% Compute general data about a sparse matrix representing the finite difference Laplacian
 close all
 clear all
 
@@ -15,3 +15,18 @@ for i =1:n
 end
 sqc = sqrt(conds)
 hs = 1./sizes;
+
+%% Compute Error for ex3b
+close all
+clear all
+
+bnorm = 811;
+for o = 4:6
+    o
+    1/pk(o)/bnorm
+end
+
+function p = pk(k)
+    lambs = [1,200,400,600,800,1000];
+    p = max(chebyshevT(k-4,lambs));
+end
